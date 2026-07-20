d3f46a038b37a248de4064df665e1364379ef003
patilom9990p <patilom9990p@gmail.com>
2026-07-20 23:52:55 +0530

    Update employeeDashboard.js

--- js/employeeDashboard.js
+++ js/employeeDashboard.js
@@ -90,7 +90,7 @@ async function verifyOfficeLocation() {
         "❌ Attendance Denied!\n\n" +
         "You are outside the office area.\n\n" +
         "Distance : " + distance.toFixed(2) + " meters.\n\n" +
-        "Your location has been recorded and sent to the Administrator."
+        ❌❌❌
     );
 
     resolve(null);
